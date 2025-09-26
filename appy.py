6d8851e2aa3812b30d1c2b5b3d71cc7b8d53252e
Nagul08 <nagul7308@gmail.com>
2025-09-26 16:42:42 +0530

    Update appy.py

--- appy.py
+++ appy.py
@@ -4,11 +4,12 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/")
+@app.route("/app")
 def home():
     return jsonify({"message": "Hello World, API is working!"})
 
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0',debug=True)
+
 
