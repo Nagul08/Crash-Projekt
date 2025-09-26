1a2154b17f5dae0c6bf6d3d5f9d5c5e815d0dd13
Nagul08 <nagul7308@gmail.com>
2025-09-26 16:48:09 +0530

    Update appy.py

--- appy.py
+++ appy.py
@@ -7,9 +7,14 @@ CORS(app)
 @app.route("/app")
 def home():
     return jsonify({"message": "Hello World, API is working!"})
+@app.route("/appy")
+def home():
+    a="hi"
+    return jsonify({"key":a,"jock":"JICK"})
 
 if __name__ == "__main__":
 
     app.run(host='0.0.0.0',debug=True)
+
 
 
