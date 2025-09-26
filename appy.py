16e5a052bbe8431826ccccbdeea9f4e7970f2da8
Nagul08 <nagul7308@gmail.com>
2025-09-26 13:59:03 +0530

    Add files via upload

---
+++ appy.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello World, API is working!"})
+
+if __name__ == "__main__":
+
+    app.run(host='0.0.0.0',debug=True)
+
